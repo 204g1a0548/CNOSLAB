ab16fd7d129d859d06b1e005fb37de48c09229e2
Likhitha A <94514469+204g1a0548@users.noreply.github.com>
2022-08-30 11:13:44 +0530

    Likhitha created sample.c

---
+++ OSLAB/sample.c
@@ -0,0 +1,7 @@
+/*
+Name of the Candidate: A. Likhitha
+Roll No: 204G1A0548
+Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+Date of Creation: 30-08-2022
+Date of Execution: 30-08-2022
+*/
