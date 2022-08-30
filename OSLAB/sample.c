e5afb6d67de2ce71278b4c9b5200318fad016d81
Likhitha A <94514469+204g1a0548@users.noreply.github.com>
2022-08-30 11:33:06 +0530

    A Likhitha completed sample.c with C source code

--- OSLAB/sample.c
+++ OSLAB/sample.c
@@ -5,3 +5,7 @@ Title of the Experiment: CPU Scheduling Using FCFS Algorithm
 Date of Creation: 30-08-2022
 Date of Execution: 30-08-2022
 */
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
